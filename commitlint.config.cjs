8a8f62c4e6512d4a9278edb4389f189eb92931db
angelespejo <angelespejo13@gmail.com>
2023-11-01 19:07:43 +0100

    :green_heart: ci: add cz-customizable

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,40 +3,11 @@
  *
  * @description Commitlint config.
  * @see https://commitlint.js.org/#/
- * @see https://github.com/conventional-changelog/commitlint
- * @see https://github.com/ngryman/cz-emoji#commitlint
  */
 
-const pkg = require( './package.json' )
-
-// Check if the user has configured the package to use conventional commits.
-const isConventional = pkg.config ? pkg.config['cz-emoji']?.conventional : false
-
-// Regex for default and conventional commits.
-const RE_DEFAULT_COMMIT      = /^(?::.*:|(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]))\s(?<emoji>\((?<scope>.*)\)\s)?.*$/gm
-const RE_CONVENTIONAL_COMMIT = /^^(?<type>\w+)(?:\((?<scope>\w+)\))?\s(?<emoji>:.*:|(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]))\s.*$/gm
-
 module.exports = {
-	rules : {
-		'cz-emoji' : [ 2, 'always' ],
+	extends : [ 'gitmoji' ],
+	rules   : {
+		'header-max-length' : [ 0, 'always', 100 ],
 	},
-	plugins : [
-		{
-			rules : {
-				'cz-emoji' : ( { raw } ) => {
-
-					const isValid = isConventional
-						? RE_CONVENTIONAL_COMMIT.test( raw )
-						: RE_DEFAULT_COMMIT.test( raw )
-
-					const message = isConventional
-						? 'Your commit message should follow conventional commit format.'
-						: 'Your commit message should be: <emoji> (<scope>)?: <subject>'
-
-					return [ isValid, message ]
-				
-				},
-			},
-		},
-	],
 }
